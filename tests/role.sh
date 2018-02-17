8ce37026e09103a4bdd430d43d9c986e30327354
Shalom Bhooshi <s.bhooshi@gmail.com>
2018-02-17 22:49:08 +0100

    Satisfy python-2.x requirement for containers

--- tests/role.sh
+++ tests/role.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+set -eu
 set -xv
 
 # setup
 if type -P apt; then
-  apt-get install -y curl
+  apt-get install -y curl python-minimal
 elif type -P zypper; then
-  zypper -n install curl
+  zypper -n install curl python
+elif type -P dnf; then
+  dnf install -y curl python
+elif type -P yum; then
+  yum install -y curl python
 fi
 
 curl https://bootstrap.pypa.io/get-pip.py | python
