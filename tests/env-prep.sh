64df84a48e3303e1c472cc097f46722be42f3257
Shalom Bhooshi <s.bhooshi@gmail.com>
2018-03-09 21:09:42 +0100

    Do not install recommended packages under apt

--- tests/env-prep.sh
+++ tests/env-prep.sh
@@ -6,7 +6,7 @@ set -xv
 # setup
 if type -P apt; then
   apt-get -qq -y update
-  apt-get install -y curl python-minimal
+  apt-get install -y --no-install-recommends curl python-minimal
 elif type -P zypper; then
   zypper -n install curl python python-xml
 elif type -P dnf; then
