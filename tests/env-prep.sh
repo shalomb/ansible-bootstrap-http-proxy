6960e0307b3da4c51ab93b6a621b2e5028f53db4
Shalom Bhooshi <s.bhooshi@gmail.com>
2020-08-11 01:38:50 +0200

    Update cache before using EPEL on RHEL

--- tests/env-prep.sh
+++ tests/env-prep.sh
@@ -24,6 +24,7 @@ elif type -P yum; then
     curl http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
       -o /tmp/epel.rpm
     rpm -ivh /tmp/epel.rpm
+    yum makecache
     yum --disablerepo=* --enablerepo=epel install -y \
       python3 python3-pip python3-setuptools
   else
