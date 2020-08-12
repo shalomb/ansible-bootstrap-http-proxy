056e40126b6ddbd066d5bbc9006a0efd7b02624f
Shalom Bhooshi <s.bhooshi@gmail.com>
2020-08-12 10:22:32 +0200

    Install cffi deps on fedora

--- tests/env-prep.sh
+++ tests/env-prep.sh
@@ -4,6 +4,8 @@ set -eu
 set -xv
 
 python3-install() {
+  source /etc/os-release
+
   if type -P apt; then
     export DEBIAN_FRONTEND=noninteractive
     apt-get -qq -y update
@@ -25,16 +27,20 @@ python3-install() {
       python3-pip        \
       python3-setuptools \
       python3-wheel
+
+    if [[ $ID == fedora ]]; then
+      $pkg install -y      \
+        gcc                \
+        libffi-devel
+    fi
   fi
 }
 
-python3 --version || python3-install
-
-if ! python3 -c 'import pip'; then
+pip3-install() {
   # We should never need to do this as supposedly pip is now
   # part of python3
   curl -fsSL https://bootstrap.pypa.io/get-pip.py | python3
-fi
+}
 
 # Workaround for when pip3 isn't setup as a command
 # This always ensures pip from python3 is used.
@@ -42,7 +48,8 @@ pip() {
   python3 -m pip "$@"
 }
 
+python3 --version       || python3-install
+python3 -c 'import pip' || pip3-install
+
 pip install --upgrade ansible
-
 ansible --version
-
